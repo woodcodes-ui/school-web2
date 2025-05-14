0753a437e2b9db2f8ed4a2a9451c89579c228bf0
woodcodes-ui <136750316+woodcodes-ui@users.noreply.github.com>
2025-05-13 20:16:51 -0400

    Add files via upload

---
+++ web.js
@@ -0,0 +1,65 @@
+const q1 = document.getElementById("q1");
+const a1 = document.getElementById("a1");
+const q2 = document.getElementById("q2");
+const a2 = document.getElementById("a2");
+const q3 = document.getElementById("q3");
+const a3 = document.getElementById("a3");
+const q4 = document.getElementById("q4");
+const a4 = document.getElementById("a4");
+const q5 = document.getElementById("q5");
+const a5 = document.getElementById("a5");
+const q6 = document.getElementById("q6");
+const a6 = document.getElementById("a6");
+const q7 = document.getElementById("q7");
+const a7 = document.getElementById("a7");
+const q8 = document.getElementById("q8");
+const a8 = document.getElementById("a8");
+const q9 = document.getElementById("q9");
+const a9 = document.getElementById("a9");
+
+
+q1.addEventListener("click", function() {
+    // Check current display property of a1 and toggle it
+    a1.style.display = (a1.style.display === "none" || a1.style.display === "") ? "grid" : "none";
+    a1.style.gap = "3px"
+});
+q2.addEventListener("click", function() {
+    // Check current display property of a1 and toggle it
+    a2.style.display = (a2.style.display === "none" || a2.style.display === "") ? "grid" : "none";
+    a2.style.gap = "3px"
+});
+q3.addEventListener("click", function() {
+    // Check current display property of a1 and toggle it
+    a3.style.display = (a3.style.display === "none" || a3.style.display === "") ? "grid" : "none";
+    a3.style.gap = "3px"
+});
+q4.addEventListener("click", function() {
+    // Check current display property of a1 and toggle it
+    a4.style.display = (a4.style.display === "none" || a4.style.display === "") ? "grid" : "none";
+    a4.style.gap = "3px"
+});
+q5.addEventListener("click", function() {
+    // Check current display property of a1 and toggle it
+    a5.style.display = (a5.style.display === "none" || a5.style.display === "") ? "grid" : "none";
+    a5.style.gap = "3px"
+});
+q6.addEventListener("click", function() {
+    // Check current display property of a1 and toggle it
+    a6.style.display = (a6.style.display === "none" || a6.style.display === "") ? "grid" : "none";
+    a6.style.gap = "3px"
+});
+q7.addEventListener("click", function() {
+    // Check current display property of a1 and toggle it
+    a7.style.display = (a7.style.display === "none" || a7.style.display === "") ? "grid" : "none";
+    a7.style.gap = "3px"
+});
+q8.addEventListener("click", function() {
+    // Check current display property of a1 and toggle it
+    a8.style.display = (a8.style.display === "none" || a8.style.display === "") ? "grid" : "none";
+    a8.style.gap = "3px"
+});
+q9.addEventListener("click", function() {
+    // Check current display property of a1 and toggle it
+    a9.style.display = (a9.style.display === "none" || a9.style.display === "") ? "grid" : "none";
+    a9.style.gap = "3px"
+});
